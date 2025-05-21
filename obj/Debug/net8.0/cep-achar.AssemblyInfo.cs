597210d7b8acc485a6db147f251a0ba343fdb6bf
samuel <samuca21santos@gmail.com>
2025-05-20 22:38:36 -0300

    CEP-API

--- obj/Debug/net8.0/cep-achar.AssemblyInfo.cs
+++ obj/Debug/net8.0/cep-achar.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cep-achar")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdfa085d318bb467ea0f8addc8e322509615fb43")]
 [assembly: System.Reflection.AssemblyProductAttribute("cep-achar")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cep-achar")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
